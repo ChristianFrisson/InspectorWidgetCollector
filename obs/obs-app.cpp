9f0e961878f1418c34eafa9417075399bc13b300
Christian Frisson <christian.frisson@gmail.com>
2016-07-18 15:07:50 +0200

    obs: use dash instead of underscore for filenames without space

--- obs/obs-app.cpp
+++ obs/obs-app.cpp
@@ -1068,7 +1068,7 @@ string GenerateTimeDateFilename(const char *extension, bool noSpace)
 			cur_time->tm_year+1900,
 			cur_time->tm_mon+1,
 			cur_time->tm_mday,
-			noSpace ? '_' : ' ',
+            noSpace ? '-' : ' ',
 			cur_time->tm_hour,
 			cur_time->tm_min,
 			cur_time->tm_sec,
@@ -1147,7 +1147,7 @@ string GenerateSpecifiedFilename(const char *extension, bool noSpace,
 	}
 
 	if (noSpace)
-		replace(sf.begin(), sf.end(), ' ', '_');
+        replace(sf.begin(), sf.end(), ' ', '-');
 
 	sf += '.';
 	sf += extension;
